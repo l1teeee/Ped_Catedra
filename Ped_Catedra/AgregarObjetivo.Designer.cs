96fa2cc02dffd595be035fa23ddee029615b6d46
Kenayred <kenayred1.7.2@gmail.com>
2024-05-13 22:40:12 -0600

    Formulario de Objetivos

---
+++ Ped_Catedra/AgregarObjetivo.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Ped_Catedra
+{
+    partial class AgregarObjetivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgregarObjetivo));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescri = new System.Windows.Forms.RichTextBox();
+            this.cmbRecordatorios = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnIngresarObjetivo = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(410, 64);
+            this.panel1.TabIndex = 16;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, -19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(89, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(318, 32);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "AGREGAR RECORDATORIO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(35, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Digite un titulo:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(38, 120);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(324, 20);
+            this.txtTitulo.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Digite una descripcion";
+            // 
+            // txtDescri
+            // 
+            this.txtDescri.Location = new System.Drawing.Point(38, 204);
+            this.txtDescri.Name = "txtDescri";
+            this.txtDescri.Size = new System.Drawing.Size(324, 96);
+            this.txtDescri.TabIndex = 20;
+            this.txtDescri.Text = "";
+            // 
+            // cmbRecordatorios
+            // 
+            this.cmbRecordatorios.FormattingEnabled = true;
+            this.cmbRecordatorios.Location = new System.Drawing.Point(38, 353);
+            this.cmbRecordatorios.Name = "cmbRecordatorios";
+            this.cmbRecordatorios.Size = new System.Drawing.Size(324, 21);
+            this.cmbRecordatorios.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "¿A que recordatorio lo va a agregar?";
+            // 
+            // btnIngresarObjetivo
+            // 
+            this.btnIngresarObjetivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnIngresarObjetivo.FlatAppearance.BorderSize = 0;
+            this.btnIngresarObjetivo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(164)))));
+            this.btnIngresarObjetivo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(164)))));
+            this.btnIngresarObjetivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresarObjetivo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresarObjetivo.ForeColor = System.Drawing.Color.White;
+            this.btnIngresarObjetivo.Location = new System.Drawing.Point(25, 409);
+            this.btnIngresarObjetivo.Name = "btnIngresarObjetivo";
+            this.btnIngresarObjetivo.Size = new System.Drawing.Size(357, 31);
+            this.btnIngresarObjetivo.TabIndex = 27;
+            this.btnIngresarObjetivo.Text = "Guardar Objetivo";
+            this.btnIngresarObjetivo.UseVisualStyleBackColor = false;
+            // 
+            // AgregarObjetivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 461);
+            this.Controls.Add(this.btnIngresarObjetivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbRecordatorios);
+            this.Controls.Add(this.txtDescri);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "AgregarObjetivo";
+            this.Text = "AgregarObjetivo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtDescri;
+        private System.Windows.Forms.ComboBox cmbRecordatorios;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnIngresarObjetivo;
+    }
+}
---
+++ Ped_Catedra/Clases/Objetivos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ped_Catedra.Clases
+{
+    public class Objetivos
+    {
+        public Objetivos() { }
+
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string descrip { get; set; }
+        public int idRecordatorio {  get; set; }
+    }
+}
